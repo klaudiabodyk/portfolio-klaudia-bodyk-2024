58a50c196e4872da0deedba7e534d1bb168f80fe
klaudiabodyk <klaudia.bodyk@gds.ey.com>
2024-03-22 21:00:30 +0100

    Revert "Revert "#4 - Add i18n library & refactor Parallax and Menu components & Add initial unit test and new Section component""
    This reverts commit 2cb9a686ad45563c803d26af768ae9e301fcc32f.

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,15 +1,17 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import { useTranslation } from 'react-i18next';
+import { navs } from "./config/menuConfig";
 import "swiper/css";
 import "swiper/css/navigation";
 import "./styles/styles.css";
-import {useState} from "react";
-import {Swiper, SwiperSlide} from "swiper/react";
-import {Navigation} from "swiper";
-import {navs} from "./config/menuConfig";
 
 const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => setIsOpen(!isOpen);
     const openClass = isOpen ? "open" : "";
+    const { t } = useTranslation();
 
     return (
         <>
@@ -18,11 +20,21 @@ const Menu = () => {
             <div className={`menu ${openClass}`}>
                 <Swiper speed={750} modules={[Navigation]} navigation>
                     {navs.map((nav) => (
-                        <SwiperSlide>{nav}</SwiperSlide>
+                        <SwiperSlide key={nav.title}>
+                            <h2>{t(nav.title)}</h2>
+                            <nav>
+                                {nav.links.map((link) => (
+                                    <a key={link.name} href={t(link.url)} style={{ animationDelay: `${0.7 + nav.links.indexOf(link) * 0.1}s` }}>
+                                        {t(link.name)}
+                                    </a>
+                                ))}
+                            </nav>
+                        </SwiperSlide>
                     ))}
                 </Swiper>
             </div>
         </>
     );
 };
+
 export default Menu;
--- src/components/parallax/Parallax.tsx
+++ src/components/parallax/Parallax.tsx
@@ -1,45 +1,41 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import gsap from "gsap";
-import { Observer } from "gsap/all";
+import {Observer} from "gsap/all";
 import "./styles/styles.css";
 import rv from "./assets/background/12.jpg";
 import mountains from "./assets/background/IMG_4671.jpeg";
 import bg1 from "./assets/gradient/9.png";
 import bg2 from "./assets/gradient/8.png";
-import me from "./assets/hi-its-me.png"
+import me from "./assets/hi_its_me.jpg";
 import Menu from "../menu/Menu";
-import { FaReact } from "react-icons/fa";
+import Section, {ISection} from "../section/Section";
+import {FaJava, FaReact} from "react-icons/fa";
 import {BiLogoTypescript} from "react-icons/bi";
 import {SiNestjs, SiRedux} from "react-icons/si";
 import {DiNodejs} from "react-icons/di";
+import {useTranslation} from 'react-i18next';
+import {RiJavascriptFill} from "react-icons/ri";
 
-type Section = {
-    title: string;
-    image: string;
-    additional? : any;
-};
-
-const sections: Section[] = [
-    { title: "Hello, I'm Klaudia", image: mountains },
-    { title: "Software developer with over 3 years of experience", image: bg2, additional: <img src={me}  alt={'me'} className="me-img"/> },
-    { title: "It's nice to meet you", image: bg1 },
-    { title: "My tech stack", image: bg2, additional:<div className="icons-space"> <FaReact /><BiLogoTypescript /> <SiRedux /></div>  },
-    { title: "In progress", image: bg1, additional:<div className="icons-space"> <DiNodejs /> <SiNestjs /></div>  },
-    { title: "My hobby", image: rv },
+const sections: ISection[] = [
+    { title: "hello", image: mountains },
+    { title: "experience", image: bg2, additional: <img src={me} alt={'me'} className="me-img"/> },
+    { title: "meet", image: bg1 },
+    { title: "techStack", image: bg2, additional: <div className="icons-space"> <FaReact /><BiLogoTypescript /> <SiRedux /> <FaJava /> <RiJavascriptFill /></div> },
+    { title: "inProgress", image: bg1, additional: <div className="icons-space"> <DiNodejs /> <SiNestjs /></div> },
+    { title: "hobby", image: rv },
 ];
 
+
 const Parallax = () => {
-    // Use the useEffect hook to initialize GSAP when the component mounts
+    const { t } = useTranslation(); // Use the t function for translations
+
     useEffect(() => {
         gsapInit();
     }, []);
 
-    // Initialize GSAP and set up the parallax effect
     const gsapInit = () => {
-        // Register the Observer plugin with GSAP
         gsap.registerPlugin(Observer);
 
-        // Select all sections and images in the DOM
         const sections = document.querySelectorAll<HTMLDivElement>("section");
         const images = document.querySelectorAll<HTMLDivElement>(".bg");
         const outerWrappers = gsap.utils.toArray<HTMLDivElement>(".outer");
@@ -48,11 +44,9 @@ const Parallax = () => {
         const wrap = gsap.utils.wrap(0, sections.length);
         let animating: boolean = false;
 
-        // Set initial styles for the outer and inner wrappers
         gsap.set(outerWrappers, { yPercent: 100 });
         gsap.set(innerWrappers, { yPercent: -100 });
 
-        // Define a function to navigate to a specific section
         const gotoSection = (index: number, direction: number) => {
             index = wrap(index);
             animating = true;
@@ -65,7 +59,6 @@ const Parallax = () => {
                 },
             });
 
-            // Animate the current section out and the new section in
             if (currentIndex >= 0) {
                 gsap.set(sections[currentIndex], { zIndex: 0 });
                 tl.to(images[currentIndex], { yPercent: -15 * dFactor }).set(
@@ -84,7 +77,6 @@ const Parallax = () => {
             currentIndex = index;
         };
 
-        // Set up the Observer to handle user interactions
         Observer.create({
             type: "wheel,touch,pointer",
             wheelSpeed: -1,
@@ -94,28 +86,14 @@ const Parallax = () => {
             preventDefault: true,
         });
 
-        // Start at the first section
         gotoSection(0, 1);
     };
 
-    // Render the sections with their titles and images
     return (
         <>
             <Menu />
             {sections.map((section) => (
-                <section key={section.title}>
-                    <div className="outer">
-                        <div className="inner">
-                            <div
-                                className="bg one"
-                                style={{ backgroundImage: `url(${section.image})` }}
-                            >
-                                <h2>{section.title}</h2>
-                                <div className={"icons"}>{section.additional}</div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <Section key={section.title} {...section} />
             ))}
         </>
     );
